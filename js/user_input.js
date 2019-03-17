2dcc01dfe2c83c131cb1bf788f64e68fbe7d431a
yuu <dptypo@gmail.com>
2019-03-18 02:25:16 +0900

    スマホテスト

--- js/user_input.js
+++ js/user_input.js
@@ -5,6 +5,31 @@ $(function() {
     });
 });
 
+$(document).ready(function() {
+    let getDevice = (function(){
+        let ua = navigator.userAgent;
+        if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+            return 'sp';
+        }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+            return 'tab';
+        }else{
+            return 'other';
+        }
+    })();
+
+    if( getDevice == 'sp' ){
+        //スマホ
+        $('.content').css('display','none');
+        $('#message').text('スマートフォンではメモリが足りないため実行できません<br>PCでの閲覧をお願いします');
+    }else if( getDevice == 'tab' ){
+        //タブレット
+        $('#message').text('メモリが不足し結果が表示できない可能性があります<br>PCでの閲覧をお願いします');
+    }else if( getDevice == 'other' ){
+        //その他
+    }
+});
+
+
 function submitText(){
     $(function() {
         window.sessionStorage.clear();
